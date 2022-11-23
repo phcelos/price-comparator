889d5b629a92a8a302923c4860aad00674c4c77d
phcelos <paulohcelos@gmail.com>
2022-11-23 19:46:17 -0300

    Create ProductViewModel.

---
+++ PriceComparator/ViewModel/ProductViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ProductViewModel.swift
+//  PriceComparator
+//
+//  Created by Paulo Henrique on 23/11/22.
+//
+
+import Foundation
+
+class ProductViewModel {
+    let product: Product
+    
+    init(product: Product) {
+        self.product = product
+    }
+}
